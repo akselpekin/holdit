254738282fc69a713420d649d72987ab42d1b4eb
Aksel Pekin <akselpekin@me.com>
2025-07-07 09:24:54 +0300

    update

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,13 +3,11 @@ import SwiftUI
 @main
 struct HoldItApp: App {
     init() {
-        // Ensure a regular Dock icon & menu bar appear
         NSApplication.shared.setActivationPolicy(.regular)
     }
 
     var body: some Scene {
         WindowGroup("holdit") {
-            // A green full-window view; define ContentView elsewhere
             Tray()
         }
         .windowStyle(HiddenTitleBarWindowStyle())
